d8407201fac67ec8e6b7ed3ff0d6e44e3dae3003
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-14 16:23:30 +0900

    Calculate CloudWatch cron expression

--- lib/wh2cwe/job.rb
+++ lib/wh2cwe/job.rb
@@ -1,10 +1,20 @@
 module Wh2cwe
   class Job
+    DOM_INDEX = 2
+    DOW_INDEX = 4
+
     attr_reader :cron, :task
 
     def initialize(cron, task)
       @cron = cron
       @task = task
+    end
+
+    def cloud_watch_cron
+      cron_fields = @cron.split(" ")
+      cron_fields[DOW_INDEX] = "?" if cron_fields[DOM_INDEX] == "*" && cron_fields[DOW_INDEX] == "*"
+      cron_fields << "*" # Year
+      cron_fields.join(" ")
     end
 
     def name(regexp)
--- spec/lib/wh2cwe/job_spec.rb
+++ spec/lib/wh2cwe/job_spec.rb
@@ -2,19 +2,47 @@ require "spec_helper"
 
 module Wh2cwe
   describe Job do
+    let(:cron) do
+      "10 0 * * *"
+    end
+
+    let(:task) do
+      "/bin/bash -l -c 'bundle exec rake create_new_companies RAILS_ENV=production'"
+    end
+
+    let(:job) do
+      Job.new(cron, task)
+    end
+
+    describe "#cloud_watch_cron" do
+      context "when the job will be invoked every day" do
+        it "should convert to CloudWatch cron expression" do
+          expect(job.cloud_watch_cron).to eq "10 0 * * ? *"
+        end
+      end
+
+      context "when the job will be invoked at the specific days" do
+        let(:cron) do
+          "10 0 1-3 * *"
+        end
+
+        it "should convert to CloudWatch cron expression" do
+          expect(job.cloud_watch_cron).to eq "10 0 1-3 * * *"
+        end
+      end
+
+      context "when the job will be invoked at the specific weekdays" do
+        let(:cron) do
+          "10 0 * * 1-3"
+        end
+
+        it "should convert to CloudWatch cron expression" do
+          expect(job.cloud_watch_cron).to eq "10 0 * * 1-3 *"
+        end
+      end
+    end
+
     describe "#name" do
-      let(:cron) do
-        "10 0 * * *"
-      end
-
-      let(:task) do
-        "/bin/bash -l -c 'bundle exec rake create_new_companies RAILS_ENV=production'"
-      end
-
-      let(:job) do
-        Job.new(cron, task)
-      end
-
       context "when valid regexp is given" do
         let(:regexp) do
           'bundle exec rake (\w+) RAILS_ENV=.*\z'
