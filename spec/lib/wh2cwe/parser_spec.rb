5ea0de2265195196df27a69341caddb01c3f897a
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-15 18:28:00 +0900

    Modify Parser.parse spec

--- spec/lib/wh2cwe/parser_spec.rb
+++ spec/lib/wh2cwe/parser_spec.rb
@@ -16,11 +16,17 @@ module Wh2cwe
       end
 
       it "should return list of jobs" do
+        [
+          { cron: "15 * * * *", command: "/bin/bash -l -c 'bundle exec rake send_awesome_mail RAILS_ENV=production'" },
+          { cron: "10 * * * *", command: "/bin/bash -l -c 'bundle exec rake update_elasticsearch RAILS_ENV=production'" },
+          { cron: "0 0 * * *", command: "/bin/bash -l -c 'bundle exec rake send_greeting_notification RAILS_ENV=production'" },
+          { cron: "10 0 * * *", command: "/bin/bash -l -c 'bundle exec rake create_new_companies RAILS_ENV=production'" },
+        ].each do |spec|
+          expect(Wh2cwe::Job).to receive(:from_crontab).with(spec[:cron], spec[:command], prefix, regexp)
+        end
+
         jobs = described_class.parse(filename, prefix, regexp)
         expect(jobs.length).to eq 4
-        jobs.each do |job|
-          expect(job).to be_an_instance_of(Wh2cwe::Job)
-        end
       end
     end
   end
