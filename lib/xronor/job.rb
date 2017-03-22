d563c9fd1cb4b7cce91c4502a37db4f2a9028f64
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-22 16:55:49 +0900

    Remove all Xronor::Job class methods

--- lib/xronor/job.rb
+++ lib/xronor/job.rb
@@ -3,29 +3,6 @@ module Xronor
     DOM_INDEX = 2
     DOW_INDEX = 4
 
-    class << self
-      def from_crontab(cron, command, prefix, regexp)
-        name = name_from_command(command, prefix, regexp)
-        schedule = cloud_watch_schedule(cron)
-
-        self.new(name, "", schedule, command)
-      end
-
-      private
-
-      def cloud_watch_schedule(cron)
-        cron_fields = cron.split(" ")
-        cron_fields[DOW_INDEX] = "?" if cron_fields[DOM_INDEX] == "*" && cron_fields[DOW_INDEX] == "*"
-        cron_fields << "*" # Year
-        "cron(#{cron_fields.join(" ")})"
-      end
-
-      def name_from_command(command, prefix, regexp)
-        matched = Regexp.new(regexp).match(command)
-        "#{prefix}#{matched ? matched[1] : ""}"
-      end
-    end
-
     def initialize(name, description, schedule, command)
       @name = name
       @description = description
--- spec/lib/xronor/job_spec.rb
+++ spec/lib/xronor/job_spec.rb
@@ -2,68 +2,6 @@ require "spec_helper"
 
 module Xronor
   describe Job do
-    describe ".from_crontab" do
-      let(:cron) do
-        "10 0 * * *"
-      end
-
-      let(:command) do
-        "/bin/bash -l -c 'bundle exec rake create_new_companies RAILS_ENV=production'"
-      end
-
-      let(:prefix) do
-        "scheduler-"
-      end
-
-      let(:regexp) do
-        ""
-      end
-
-      let(:job) do
-        described_class.from_crontab(cron, command, prefix, regexp)
-      end
-
-      context "when valid regexp is given" do
-        let(:regexp) do
-          'bundle exec rake (\w+) RAILS_ENV=.*\z'
-        end
-
-        it "should calculate job name using the given regexp" do
-          expect(job.name).to eq "scheduler-create_new_companies"
-        end
-      end
-
-      context "when invalid regexp is given" do
-        let(:regexp) do
-          '\Abundle exec rake (\w+) RAILS_ENV=.*\z'
-        end
-
-        it "should return empty string" do
-          expect(job.name).to eq "scheduler-"
-        end
-      end
-
-      context "when the job will be invoked at the specific days" do
-        let(:cron) do
-          "10 0 1-3 * *"
-        end
-
-        it "should convert to CloudWatch cron expression" do
-          expect(job.schedule).to eq "cron(10 0 1-3 * * *)"
-        end
-      end
-
-      context "when the job will be invoked at the specific weekdays" do
-        let(:cron) do
-          "10 0 * * 1-3"
-        end
-
-        it "should convert to CloudWatch cron expression" do
-          expect(job.schedule).to eq "cron(10 0 * * 1-3 *)"
-        end
-      end
-    end
-
     let(:name) do
       "scheduler-production-create_new_companies"
     end
