5fe42772d5738460287c67d148f9660f54cc2349
Daisuke Fujita <dtanshi45@gmail.com>
2017-03-23 12:05:32 +0900

    Process template per job

--- lib/xronor/generator/erb.rb
+++ lib/xronor/generator/erb.rb
@@ -7,6 +7,17 @@ module Xronor
           erb = open(options[:template]).read
           ::ERB.new(erb, nil, "-").result(binding)
         end
+
+        def generate_per_job(filename, options)
+          jobs = Xronor::Parser.parse(filename)
+          erb = open(options[:template]).read
+
+          jobs.inject({}) do |result, job|
+            @job = job
+            result[job.name.gsub(/[^\.\-_A-Za-z0-9]/, "_").downcase] = ::ERB.new(erb, nil, "-").result(binding)
+            result
+          end
+        end
       end
     end
   end
--- spec/lib/xronor/generator/erb_spec.rb
+++ spec/lib/xronor/generator/erb_spec.rb
@@ -38,6 +38,47 @@ module Xronor
           EOS
         end
       end
+
+      describe ".generate_per_job" do
+        let(:filename) do
+          fixture_path("schedule.rb")
+        end
+
+        let(:template) do
+          fixture_path("template_single.erb")
+        end
+
+        let(:options) do
+          {
+            template: template,
+          }
+        end
+
+        it "should process ERB template" do
+          expect(described_class.generate_per_job(filename, options)).to eq({
+            "send_awesome_mails" => <<-EOS,
+# Send awesome mails - Send awesome mails
+15 * * * * /bin/bash -l -c 'bundle exec rake send_awesome_mail RAILS_ENV=production'
+ EOS
+            "update_elasticsearch_indices" => <<-EOS,
+# Update Elasticsearch indices - Update Elasticsearch indices
+10 * * * * /bin/bash -l -c 'bundle exec rake update_elasticsearch RAILS_ENV=production'
+EOS
+            "send_greeting_notifications" => <<-EOS,
+# Send greeting notifications - Send greeting notifications for all users
+0 15 * * * /bin/bash -l -c 'bundle exec rake send_greeting_notification RAILS_ENV=production'
+EOS
+            "create_new_companies" => <<-EOS,
+# Create new companies - Create new companies
+10 15 * * 2 /bin/bash -l -c 'bundle exec rake create_new_companies RAILS_ENV=production'
+EOS
+            "healthcheck" => <<-EOS,
+# Healthcheck - Healthcheck
+0 10 10,20 * * /bin/bash -l -c 'bundle exec rake ping RAILS_ENV=production'
+EOS
+          })
+        end
+      end
     end
   end
 end
